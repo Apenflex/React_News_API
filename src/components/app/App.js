b3b5a03680a9cbcb9291eed729627ebaa68c4e77
Sviatoslav <blackdbf@gmail.com>
2023-02-03 18:48:30 +0200

    start project

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+   return (
+      <div className="app">
+         <h1>Hello News</h1>
+         <p> Here in future will be news...</p>
+         <a href="/" className="button__main inner">Click</a>
+      </div>
+   );
+}
+
+export default App;
